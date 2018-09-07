ffe7b4a322bca557ae50c78a6ca57a4a0d91ba36
juanroalvarado <juanalvarado@ufm.edu>
2018-09-06 19:11:04 -0600

    blessings3

--- matching/matching.py
+++ matching/matching.py
@@ -26,6 +26,9 @@ def matching(category):
         ids_list= ids_list + str(result[0]) +','
         bid_list = bid_list + str(result[1]) +','
 
+    ids_list = ids_list[:-1]
+    bid_list = bid_list[:-1]
+    
     dd = {"campaign_ids": ids_list,
             "bids": bid_list}
     cnx.close()
